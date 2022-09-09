de0df4b7d4a664643c40d22c4825345f19c06708
OseSam998 <osewe8625@gmail.com>
2022-09-09 04:40:44 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description:using main function
+*this program prints"Programming is like building a multilingual puzzle"
+*Return (0)
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	flot f;
+	
+
+	printf("size of a char: %1d bite(s)\m" ,slzeof(c));
+
+
+
